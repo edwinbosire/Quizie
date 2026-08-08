c957a261a6ab129bb10c974a11463751011a22b5
Edwin Bosire <me@edwinb.co.uk>
2026-08-08 16:35:43 +0100

    Add previews for flashcard and match game home cards

--- Quizie/Features/Flashcards/Views/FlashcardHomeCard.swift
+++ Quizie/Features/Flashcards/Views/FlashcardHomeCard.swift
@@ -24,3 +24,27 @@ struct FlashcardHomeCard: View {
         return Int((Double(knownCount) / Double(reviewedCount) * 100).rounded())
     }
 }
+
+// MARK: - Previews
+
+#Preview("Flashcard Home Card - New") {
+    let defaults = UserDefaults(suiteName: "FlashcardHomeCardPreview.New")!
+    defaults.set(0, forKey: StudyStatistics.flashcardsReviewedKey)
+    defaults.set(0, forKey: StudyStatistics.flashcardsKnownKey)
+
+    return FlashcardHomeCard(onOpen: {})
+        .padding()
+        .background(Color.hbBackground)
+        .defaultAppStorage(defaults)
+}
+
+#Preview("Flashcard Home Card - Progress") {
+    let defaults = UserDefaults(suiteName: "FlashcardHomeCardPreview.Progress")!
+    defaults.set(42, forKey: StudyStatistics.flashcardsReviewedKey)
+    defaults.set(34, forKey: StudyStatistics.flashcardsKnownKey)
+
+    return FlashcardHomeCard(onOpen: {})
+        .padding()
+        .background(Color.hbBackground)
+        .defaultAppStorage(defaults)
+}
